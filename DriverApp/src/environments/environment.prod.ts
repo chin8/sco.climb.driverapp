50a7aa7535bf1313fcded73c3c8e4cef374192a4
Matteo Chini <matteo.chini@gmail.com>
2024-02-02 11:47:29 +0100

    new environment files

---
+++ DriverApp/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true
+};
+
+export const SERVER_URL = 'https://devServer.com/api';
+export const VUE_APP_I18N_LOCALE="it"
+export const VUE_APP_I18N_FALLBACK_LOCALE="it"
